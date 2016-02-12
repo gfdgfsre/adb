ba0e0ce5c1f85aec7d3f6a929943791fa462055d
Simon Fels <simon.fels@canonical.com>
2016-02-12 07:43:59 +0100

    Imported Upstream version 5.1.1r34+git20160211

--- core/libsparse/defs.h
+++ core/libsparse/defs.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007 The Android Open Source Project
+ * Copyright (C) 2014 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,19 +14,10 @@
  * limitations under the License.
  */
 
-#ifndef __CUTILS_ZYGOTE_H
-#define __CUTILS_ZYGOTE_H
+#ifndef _LIBSPARSE_DEFS_H_
 
-#ifdef __cplusplus
-extern "C" {
+#ifndef __unused
+#define __unused        __attribute__((__unused__))
 #endif
 
-int zygote_run_oneshot(int sendStdio, int argc, const char **argv);
-int zygote_run(int argc, const char **argv);
-int zygote_run_wait(int argc, const char **argv, void (*post_run_func)(int));
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* __CUTILS_ZYGOTE_H */
+#endif /* _LIBSPARSE_DEFS_H_ */
